b63584d2782353e62acb1e1e2c4d533759ffd8b3
muhimin <62156192+muhimin01@users.noreply.github.com>
2024-06-11 23:50:01 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
@@ -8,25 +9,41 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
         const request = data.toString();
-        console.log(`Request: ${request}`);
         const url = request.split(' ')[1];
         const headers = request.split('\r\n');
+        
+        console.log(`Request: ${request}`);
 
-        function response(content) {
+        function response(contentType, content) {
             console.log(content);
-            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: ${contentType}\r\nContent-Length: ${content.length}\r\n\r\n${content}`)
         }
 
-        if (url == "/") {
+        function notfound() {
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+        }
+
+        if (url === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
         } else if (url.includes("/echo/")) {
             const echo = url.split("/echo/")[1];
-            response(echo);
-        } else if (url == "/user-agent") {
+            response("text/plain", echo);
+        } else if (url === "/user-agent") {
             const userAgent = headers[2].split('User-Agent: ')[1];
-            response(userAgent);
+            response("text/plain", userAgent);
+        } else if (url.includes("/files/")) {
+            const directory = process.argv[3];
+            const filename = url.split("/files/")[1];
+            
+            if (fs.existsSync(`${directory}/${filename}`)) {
+                const file = fs.readFileSync(`${directory}/${filename}`).toString();
+                response("application/octet-stream", file);
+            } else {
+                notfound();
+            }
+
         } else {
-            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+            notfound();
         }
     });
 

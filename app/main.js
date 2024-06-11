eb1c8a7b3b21d9bab49cd02cb00be6c525a7a6b8
muhimin <62156192+muhimin01@users.noreply.github.com>
2024-06-12 00:44:07 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -4,13 +4,28 @@ const fs = require("fs");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const parseRequest = (requestData) => {
+    const request = requestData.toString().split("\r\n");
+    const [method, url, protocol] = request[0].split(" ");
+    const headers = {};
+
+    request.slice(1).forEach((header) => {
+        const [key, value] = header.split(" ");
+
+        if (key && value) {
+            headers[key] = value;
+        }
+    });
+
+    return { method, url, protocol, headers };
+
+};
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        const request = data.toString();
-        const url = request.split(' ')[1];
-        const headers = request.split('\r\n');
+        const request = parseRequest(data);
+        const { method, url, protocol, headers } = request;
 
         console.log(`Request: ${request}`);
 
@@ -25,24 +40,33 @@ const server = net.createServer((socket) => {
 
         if (url === "/") {
             socket.write("HTTP/1.1 200 OK\r\n\r\n");
-        } else if (url.includes("/echo/")) {
+        } else if (url.startsWith("/echo/")) {
             const echo = url.split("/echo/")[1];
             response("text/plain", echo);
-        } else if (url === "/user-agent") {
+        } else if (url.startsWith("/user-agent/")) {
             const userAgent = headers[2].split('User-Agent: ')[1];
             response("text/plain", userAgent);
-        } else if (url.includes("/files/")) {
+        } else if (url.startsWith("/files/" && method === "GET")) {
             const directory = process.argv[3];
-            const filename = url.split("/files/")[1];
+            const fileName = url.split("/files/")[1];
 
-            if (fs.existsSync(`${directory}/${filename}`)) {
-                const file = fs.readFileSync(`${directory}/${filename}`).toString();
+            if (fs.existsSync(`${directory}/${fileName}`)) {
+                const file = fs.readFileSync(`${directory}/${fileName}`).toString();
                 response("application/octet-stream", file);
             } else {
                 notfound();
             }
 
-        } else {
+        } else if (url.startsWith("/files/") && method === "POST") {
+            const fileName = url.split("/")[2];
+            const filePath = FILES_DIR + fileName;
+            const file = data.toString("utf-8").split("\r\n\r\n")[1];
+            
+            fs.writeFileSync(filePath, file);
+            socket.write("HTTP/1.1 201 Created\r\n\r\n")
+        }
+
+        else {
             notfound();
         }
     });

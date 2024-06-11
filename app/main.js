49a202379ea1f0500cf48e319b28a5ed1d9d9138
muhimin <62156192+muhimin01@users.noreply.github.com>
2024-06-11 22:40:32 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -6,18 +6,23 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
     socket.on("data", (data) => {
-        const request = data.toString();
-        if (request.startsWith("GET / ")) {
-            const httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
-            socket.write(httpResponse);
+        const url = data.toString();
+        if (url.startsWith("GET / ")) {
+            if (url.includes("/echo/")) {
+                const content = url.split("/echo/")[1];
+                const httpResponse = `Content-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n`;
+                socket.write(`HTTP/1.1 200 OK\r\n${httpResponse}\r\n${content}`);
+            }
+            else {
+                socket.write("HTTP/1.1 200 OK\r\n\r\n");
+            }
         }
         else {
-            const httpResponse = "HTTP/1.1 404 Not Found\r\n\r\n";
-            socket.write(httpResponse);
+            socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
         socket.end();
     });
-    
+
     socket.on("close", () => {
         socket.end();
     });
